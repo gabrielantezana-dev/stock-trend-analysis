71df8ad10218affb5ebdda6a9bea306cc421d843
gabrielantezana-dev <gabriel.antezana20@gmail.com>
2026-07-07 22:20:56 -0500

    Initialize stock trend analysis module with data retrieval and plotting

--- stock-trend-analysis/stock_intialize.py
+++ stock-trend-analysis/stock_intialize.py
@@ -1,1 +1,12 @@
-print("Intializing stock trend analysis module")
+import yfinance as yf
+import matplotlib.pyplot as plt
+ticker_symbol = "AAPL"
+
+ticker = yf.Ticker(ticker_symbol)
+
+stock_data = ticker.history(period="5y")
+print("Apple Stock Data: ")
+print(stock_data)
+
+stock_data['Close'].plot(kind='line', color='orange')
+plt.show()
