feccd38025dae10d22d4392a121858a55d4bd69b
gabrielantezana-dev <gabriel.antezana20@gmail.com>
2026-07-10 16:21:30 -0500

    Refactor stock data retrieval to handle multiple tickers and improve plotting with labels and specific hovering labels

--- stock-trend-analysis/stock_intialize.py
+++ stock-trend-analysis/stock_intialize.py
@@ -1,12 +1,39 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
-ticker_symbol = "AAPL"
+import mplcursors
 
-ticker = yf.Ticker(ticker_symbol)
+ticker_symbols = ["AAPL", "MSFT", "NVDA", "TSLA", "AMZN"]
 
-stock_data = ticker.history(period="5y")
-print("Apple Stock Data: ")
+tickers = []
+stock_data = []
+lines = []
+
+for x in range(5):
+    ticker = yf.Ticker(ticker_symbols[x])
+    tickers.append(ticker)
+print(tickers)
+
+for x in range(5):
+    stock_par_data = tickers[x].history(period="5y")
+    stock_data.append(stock_par_data)
+
+print("Stock Data: ")
 print(stock_data)
 
-stock_data['Close'].plot(kind='line', color='orange')
+plt.figure(figsize=(15, 4))
+# THIS IS FOR ONE SINGLE GRAPH
+
+for x in range(5):
+    line = stock_data[x]['Close'].plot(kind='line', label=ticker_symbols[x])
+    lines.append(line)
+
+mplcursors.cursor(hover=True)
+
+plt.legend()
 plt.show()
+
+# THIS IS FOR SEPERATE GRAPHS
+
+# for x in range(5):
+#     stock_data[x]['Close'].plot(kind='line', color='orange')
+#     plt.show()
